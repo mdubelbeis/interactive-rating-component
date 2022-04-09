d14d9ea41ce769bc0022c26d4d8fd530293e27f7
mdubelbeis <mason.dubelbeis@gmail.com>
2022-04-08 22:46:16 -0400

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  theme: {
+    colors: {
+      white: 'hsl(0, 0%, 100%)',
+      'light-grey': 'hsl(217, 12%, 63%)',
+      'medium-grey': 'hsl(216, 12%, 54%)',
+      'dark-blue': 'hsl(213, 19%, 18%)',
+      'darker-blue': 'hsl(216, 12%, 8%)',
+      'bright-orange': '#FC7614',
+      'gradient-black-start': '#232A34',
+      'gradient-black-end': '#181E27',
+    },
+    fontFamily: {
+      sans: ['Overpass', 'sans-serif'],
+    },
+    fontSize: {
+      p: '15px',
+    },
+    extend: {},
+  },
+  plugins: [],
+};
