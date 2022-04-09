2068f8e6c0ba2c4149ea60bba78be4092b7d2182
mdubelbeis <mason.dubelbeis@gmail.com>
2022-04-08 23:13:59 -0400

    added state to app component

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,15 @@
+import { useState } from 'react';
+
 import Rate from './Rate';
 import Thanks from './Thanks';
 
 function App() {
+  const [submitted, setSubmitted] = useState(false);
+  const [rating, setRating] = useState(null);
   return (
-    <div className="App">
-      <Rate />
-      <Thanks />
+    <div className="flex">
+      {!submitted && <Rate />}
+      {submitted && <Thanks />}
     </div>
   );
 }
